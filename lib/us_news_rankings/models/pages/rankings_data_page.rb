31df9dbbc3cfb41f003ac5d5a64ab53aa24e9227
MJ Rossetti <datacreativellc@gmail.com>
2018-02-11 18:16:36 -0500

    Remove all the things

--- lib/us_news_rankings/models/pages/rankings_data_page.rb
+++
@@ -1,7 +0,0 @@
-module UsNewsRankings
-  class RankingsDataPage < Page
-    def table
-      @table ||= document.at_css("table.ranking-data")
-    end
-  end
-end
--- lib/us_news_rankings/models/pages/search_results_page.rb
+++
@@ -1,7 +0,0 @@
-module UsNewsRankings
-  class SearchResultsPage < Page
-    def table
-      @table ||= document.at_css("table.searchresult")
-    end
-  end
-end
