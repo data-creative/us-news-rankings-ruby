45c7e4a9756c500607c05ec4816345f75becb03f
MJ Rossetti <datacreativellc@gmail.com>
2018-04-18 01:19:52 -0400

    Expand usage of mocks for consistency

--- spec/annual_list_spec.rb
+++ spec/annual_list_spec.rb
@@ -2,17 +2,17 @@ RSpec.describe UsNewsRankings::AnnualList do
   let(:category){ UsNewsRankings::Education::GraduateSchools::LawClinical::Category.new }
   let(:annual_list){ UsNewsRankings::Education::GraduateSchools::LawClinical::AnnualList.new(2017) }
 
-  describe "#source_urls" do
-    before(:each) do
-      allow(category).to receive(:source_urls).and_return(
-        {
-          2017=>["www.google.com/2017", "www.google.com/2017?page=2", "www.google.com/2017?page=3"],
-          2016=>["www.google.com/2016", "www.google.com/2016?page=2"]
-        }
-      )
-      allow(annual_list).to receive(:category).and_return(category)
-    end
+  before(:each) do
+    allow(category).to receive(:source_urls).and_return(
+      {
+        2017=>["www.google.com/2017", "www.google.com/2017?page=2", "www.google.com/2017?page=3"],
+        2016=>["www.google.com/2016", "www.google.com/2016?page=2"]
+      }
+    )
+    allow(annual_list).to receive(:category).and_return(category)
+  end
 
+  describe "#source_urls" do
     it "should return an array of urls for the given year" do
       expect(annual_list.source_urls).to match_array(["www.google.com/2017", "www.google.com/2017?page=2", "www.google.com/2017?page=3"])
     end
@@ -20,7 +20,7 @@ RSpec.describe UsNewsRankings::AnnualList do
 
   describe "#pages" do
     it "should return a list of page objects representing source content" do
-      expect(annual_list.pages.count).to eql(1)
+      expect(annual_list.pages.count).to eql(3)
     end
   end
 
